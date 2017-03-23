fd98db79fcaa08e90023cc804169603899292626
Flavio Cassini <fbcassini@fullsail.edu>
2017-03-22 23:58:31 -0400

    fixed cosole.logs and added incrementer

--- index.js
+++ index.js
@@ -16,12 +16,38 @@ function debug(info, status) {
     const date = new Date();
     const timestamp = '[' + date.toISOString() + '] ';
 
-    fs.appendFile('src/logs/log.log', '\n' + timestamp + info, (err) => {
+    console.log(timestamp + info, (err) => {
       if (err !== null) { // If there is an error, write the error to the log.
-        fs.appendFile('src/logs/log.log', '\n' + err);
+        console.error(err);
       }
     });
   }
 }
+
+
+//Version Number incrementer //Flavio B Cassini
+
+exports.verNumberInc = (currentVersionNumber, change) => {
+  const versionArray = currentVersionNumber.split('.');
+
+  for(let versionIndex in versionArray) {
+    versionArray[versionIndex] = parseInt(versionArray[versionIndex]);
+  }
+
+  if(change === 'major') {
+      versionArray[0] += 1;
+      versionArray[1] = 0;
+      versionArray[2] = 0;
+    } else if(change === 'minor') {
+      versionArray[1] += 1;
+      versionArray[2] = 0;
+    } else if(change === 'patch') {
+      versionArray[2] += 1;
+    }
+
+    return versionArray.join('.');
+}
+
+
 
 exports.debug = debug;
